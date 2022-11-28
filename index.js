32692ab3f89f6b49639c77c19ee1ec8349ccf921
rocky-islam <62227570+rocky-islam@users.noreply.github.com>
2022-11-28 16:32:21 +0600

    user role add done

--- index.js
+++ index.js
@@ -195,6 +195,26 @@ async function run(){
             res.send(user);
         })
 
+        // set admin roll
+        app.get('/users/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const query = {email}
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin: user?.role === 'admin'});
+        })
+
+
+        // set seller roll
+        app.get('/users/seller/:email', async(req, res) =>{
+            const email = req.params.email;
+            const query = {email}
+            const user = await usersCollection.findOne(query);
+            res.send({isSeller: user?.role === 'seller'});
+        })
+
+
+
+
 
     }
     finally{
